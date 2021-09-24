d448dec7692bba68a88bc3c582deebb12f934924
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-09-24 14:17:13 +0200

    Increase minimum Boost version to 1.77 on Windows

--- scripts/install_deps.ps1
+++ scripts/install_deps.ps1
@@ -12,9 +12,12 @@ if ( -not (Test-Path "$PSScriptRoot\..\deps\boost") ) {
 
   # FIXME: The default user agent results in Artifactory treating Invoke-WebRequest as a browser
   # and serving it a page that requires JavaScript.
-  Invoke-WebRequest -URI "https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source/boost_1_74_0.zip" -OutFile boost.zip -UserAgent ""
+  Invoke-WebRequest -URI "https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.zip" -OutFile boost.zip -UserAgent ""
+  if (-not (Get-FileHash boost.zip).Hash -eq "15dc0502666851226f1d9c0fe352ccaf0ffdeff2350b6d2d08a90fcd1f610a10"))
+    throw 'Downloaded Boost source package has wrong checksum.'
+  }
   tar -xf boost.zip
-  cd boost_1_74_0
+  cd boost_1_77_0
   .\bootstrap.bat
   .\b2 -j4 -d0 link=static runtime-link=static variant=release threading=multi address-model=64 --with-filesystem --with-system --with-program_options --with-test --prefix="$PSScriptRoot\..\deps\boost" install
   if ( -not $? ) { throw "Error building boost." }
